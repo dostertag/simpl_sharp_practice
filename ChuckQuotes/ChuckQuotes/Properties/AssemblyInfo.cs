73bbd16a06f2355e85ea30d7a40f055d12161455
daniel ostertag <k.d.ostertag@gmail.com>
2018-02-17 13:14:04 -0600

    added cs file to GoL new testfile

---
+++ ChuckQuotes/ChuckQuotes/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ChuckQuotes")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChuckQuotes")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
