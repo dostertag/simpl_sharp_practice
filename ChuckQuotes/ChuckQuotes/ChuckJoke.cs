a353c8ae1ba77a68cae4031ffa74ac0a8096feaa
daniel ostertag <k.d.ostertag@gmail.com>
2018-02-17 14:03:29 -0600

    base class added -- still have to work out http request to string

---
+++ ChuckQuotes/ChuckQuotes/ChuckJoke.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;
+using System.Net;
+using Newtonsoft.Json;
+using Crestron.SimplSharp.CrestronIO;                          				// For Basic SIMPL# Classes
+
+namespace ChuckQuotes
+{
+    public class ChuckJoke
+    {
+
+        /// <summary>
+        /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
+        /// use an Initialize method
+        /// </summary>
+        public ChuckJoke()
+        {
+        }
+
+        public string GetRandomJoke()
+        {
+            string jsonResponse = string.Empty;
+            string uri = @"http://api.icndb.com/jokes/random";
+
+            jsonResponse = Get(uri);
+            ChuckSays currentChuck = deserializeJSON(jsonResponse);
+            return currentChuck.value.joke;
+
+            
+        }
+
+        private static string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+
+        }
+
+        private static ChuckSays deserializeJSON(string json)
+        {
+            try
+            {
+                ChuckSays response = JsonConvert.DeserializeObject<ChuckSays>(json);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error creating Chuck: " + ex.Message.ToString());
+
+            }
+            return null;
+        }
+    }
+}
---
+++ ChuckQuotes/ChuckQuotes/ChuckSays.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace ChuckQuotes
+{
+    public class ChuckSays
+    {
+        public string type { get; set; }
+        public Joke value { get; set; }
+
+        public class Joke
+        {
+            public int id { get; set; }
+            public string joke { get; set; }
+            public string[] categories { get; set; }
+        }
+    }
+}
