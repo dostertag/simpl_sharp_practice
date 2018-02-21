9ccb9c47b9e9eb4cd4b946e5ff0eda92df376ac1
Daniel Ostertag <k.d.ostertag@gmail.com>
2018-02-20 20:31:25 -0600

    Add files via upload

--- ChuckQuotes/ChuckQuotes/ChuckJoke.cs
+++ ChuckQuotes/ChuckQuotes/ChuckJoke.cs
@@ -11,6 +11,8 @@ namespace ChuckQuotes
     public class ChuckJoke
     {
 
+        public string randomJoke;
+
         /// <summary>
         /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
         /// use an Initialize method
@@ -18,35 +20,63 @@ namespace ChuckQuotes
         public ChuckJoke()
         {
         }
-
+        /*
         public string GetRandomJoke()
         {
-            string uri = @"http://api.icndb.com/jokes/random";
-            return GetAJoke(uri);
+            string chucksURL = @"http://api.icndb.com/jokes/random.json";
+            string randomJoke = String.Empty;
+            randomJoke = GetAJoke(chucksURL);
+            return returnedJoke;
             
             
         }
+         * */
         /// <summary>
         /// sample code found here:
         /// https://github.com/plinck/CrestronSSClass/blob/master/SimplSharpDay3_Json/SimplSharpDay3_Json/ControlSystem.cs
         /// </summary>
         /// <param name="uri"></param>
         /// <returns></returns>
-        private static string GetAJoke(string uri)
+        public void GetAJoke()
         {
+            string chucksURL = @"http://api.icndb.com/jokes/random.json";
+
+            try
+            {
+                var httpSet = new HttpClient();
+                httpSet.KeepAlive = false;
+                httpSet.Accept = "text/html";
+                HttpClientRequest sRequest = new HttpClientRequest();
+                sRequest.Url.Parse(chucksURL);
+                HttpClientResponse sResponse = httpSet.Dispatch(sRequest);
+                var jsontext = sResponse.ContentString;
+
+                ChuckSays currentChuck = JsonConvert.DeserializeObject<ChuckSays>(jsontext);
+                randomJoke = currentChuck.value.joke;
+
+            }
+            catch (Exception e)
+            {
+                CrestronConsole.PrintLine("couldn't execute: {0}", e);
+            }
+
+            /*
             string response = string.Empty;
             HttpClient client = new HttpClient();
+            // response = client.Get(uri);
+            CrestronConsole.Print(response);
             HttpClientResponse httpResponse;
             HttpClientRequest httpRequest = new HttpClientRequest();
             client.TimeoutEnabled = true;
             client.Timeout = 5;
             client.KeepAlive = false;
-            httpRequest.Url.Parse(uri);
+            httpRequest.Url.Parse(someURL);
             httpResponse = client.Dispatch(httpRequest);
 
             ChuckSays currentChuck = deserializeJSON(httpResponse.ContentString);
             
             return currentChuck.value.joke;
+             * */
         }
 
 
@@ -55,13 +85,16 @@ namespace ChuckQuotes
             try
             {
                 ChuckSays response = JsonConvert.DeserializeObject<ChuckSays>(json);
+                string tmp = response.value.joke;
+                CrestronConsole.ConsoleCommandResponse(tmp);
                 return response;
             }
             catch (Exception ex)
             {
-                CrestronConsole.ConsoleCommandResponse("Error creating chuck!");
+                CrestronConsole.PrintLine("Error creating chuck! {0}", ex);
                 return null;                
             }
         }
     }
+    
 }
